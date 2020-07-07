c9ab21872c1ce92a78e74fb6c2b5102c22c14c12
leif160519 <yzluofei@126.com>
2020-07-07 15:29:03 +0800

    新增mysql

---
+++ mysql/mysql.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash 
+config_dir=`pwd`
+PASSWORD=root
+docker run -d  --name mysql\
+     -p 3306:3306 \
+     -e MYSQL_ROOT_PASSWORD=$PASSWORD \
+     -e TZ=Asia/Shanghai \
+     --restart=always \
+     -v ${config_dir}/mysqld.cnf:/etc/mysql/mysql.conf.d/mysqld.cnf \
+     mysql
+exit
